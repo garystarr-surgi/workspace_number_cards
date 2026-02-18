25e679edecc7394a0a74259e3a98360f3d51ebc3
garystarr-surgi <gary.starr@surgishop.com>
2026-02-18 09:38:20 -0500

    Update api.py

--- workspace_number_cards/api.py
+++ workspace_number_cards/api.py
@@ -1,1 +1,16 @@
+import frappe
+
+@frappe.whitelist()
+def open_sales_orders_count():
+    count = frappe.db.count(
+        "Sales Order",
+        {
+            "docstatus": 0,              # draft
+            "owner": frappe.session.user  # logged in user
+        }
+    )
+
+    return {
+        "value": count
+    }
 
